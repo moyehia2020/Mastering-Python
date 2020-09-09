54c5771207b21a0bec7f7d02259c2e8d9896d1e4
Mo-yehia <mo-yehia@Mo-yehias-MacBook-Air.local>
2020-09-09 12:31:44 +0300

    This Is The First Assignment

---
+++ Assignment_1.py
@@ -0,0 +1,27 @@
+#------------------------------------------
+# My First Python Course
+# This is My First Homework
+# Elzero Web School by Osama Mohamed
+#------------------------------------------
+
+my_firstName = "mohamed"      # This is My First Name
+
+my_secondName = "yehia"       # This is My Second Name
+
+my_age = 39                   # This is My Age
+
+my_skill1 = "Graphic Design"  # My First Skill
+
+my_skill2 = "novel writing"   # My second Skill
+
+my_skill3 = "marketing"       # My third Skill
+
+
+print(my_firstName)
+print(my_secondName)
+print(my_age)
+print(my_skill1)
+print(my_skill2)
+print(my_skill3)
+
+print(my_firstName, my_secondName, my_age, my_skill1, my_skill2, my_skill2)
